b25f8ce5268ec45b629ab2ab3657d05ae3e1edf5
Alexey Shemyakin <2alexeysh@gmail.com>
2019-02-06 17:33:01 +0300

    Задание Стиль и Идиомы

---
+++ lesson8/carriage.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative 'maker.rb'
+require_relative 'valid.rb'
+class Carriage
+  include Maker
+  include Valid
+
+  attr_reader :free_space
+
+  def initialize(full_space)
+    @full_space = full_space
+    @free_space = @full_space
+    validate!
+  end
+
+  def occupy_space(load_space = 1)
+    @free_space -= load_space if valid_load?(load_space)
+  end
+
+  def occupied_space
+    @full_space - free_space
+  end
+
+  private
+
+  def valid_load?(load_space)
+    load_space.positive? && (free_space - load_space).positive?
+  end
+
+end
